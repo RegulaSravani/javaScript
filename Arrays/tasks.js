4fb38b8884eec811ef3086a05a58a1e63b4e1ad3
Krishna Mandava <mandava.krishna777@gmail.com>
2022-08-08 17:18:17 +0530

    Array tasks

---
+++ Arrays/tasks.js
@@ -0,0 +1,21 @@
+const persons = [
+  {
+    name: 'Ram',
+    age: 23,
+  },
+  {
+    name: 'Sandeep',
+    age: 12,
+  },
+  {
+    name: 'Sowmya',
+    age: 32,
+  },
+  {
+    name: 'Ramesh',
+    age: 27,
+  },
+];
+
+//1) Modify the age of sowmya in differnet ways
+//2) Find the younger and older persons from the given persons array
